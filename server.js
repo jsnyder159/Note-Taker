1eddc67f98b203f1f83220cad0067728b2cdf9c8
Jason Snyder <jason.snyder159@gmail.com>
2022-03-20 18:22:36 -0400

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const fs = require('fs');
 const path = require('path');
 const api = require('./routes/notes.js');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,7 +10,7 @@ const html = require('./routes/index.js')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use('/', html)
+app.use('/', html);
 app.use('/api', api);
 
 app.use(express.static('public'));
